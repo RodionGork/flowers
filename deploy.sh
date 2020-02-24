e7c508f389f796bcbef9a70f593f5a4485a43b24
Rodion Gorkovenko <rodion_gork@mail.ru>
2020-02-24 20:27:18 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,8 @@ cp -r src /tmp/src
 git fetch origin gh-pages
 git checkout FETCH_HEAD
 cp /tmp/src/* .
+echo $KKK_SECRET | base64 --decode >/tmp/flower.pri.gz
+gunzip /tmp/flower.pri.gz
+ls -l /tmp
 ls -l
 echo "Done!"
