47c0168806767c765211d2c2d49bd8e759a7a63b
Rodion Gorkovenko <rodion_gork@mail.ru>
2020-02-24 21:22:19 +0300

    Updated deploy.sh with proper branch name

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 cp -r src /tmp/src
 git fetch origin gh-pages
 git checkout FETCH_HEAD
+git checkout -b gh-pages
 cp /tmp/src/* .
 git add -A
 t=(date +%s)
@@ -8,5 +9,5 @@ git commit -m "Auto update $t"
 echo $KKK_SECRET | base64 --decode >/tmp/flower.pri.gz
 gunzip /tmp/flower.pri.gz
 chmod 600 /tmp/flower.pri
-GIT_SSH_COMMAND="ssh -i /tmp/flower.pri" git push
+GIT_SSH_COMMAND="ssh -i /tmp/flower.pri" git push --set-upstream origin gh-pages
 echo "Done!"
