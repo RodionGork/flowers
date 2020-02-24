989f3909da26b7ff5b1ed9485e4f7798f390e3c8
Rodion Gorkovenko <rodion_gork@mail.ru>
2020-02-24 20:34:28 +0300

    Attempt for push

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,11 @@ cp -r src /tmp/src
 git fetch origin gh-pages
 git checkout FETCH_HEAD
 cp /tmp/src/* .
+git add -A
+t=(date +%s)
+git commit -m "Auto update $t"
 echo $KKK_SECRET | base64 --decode >/tmp/flower.pri.gz
 gunzip /tmp/flower.pri.gz
-ls -l /tmp
-ls -l
+chmod 600 /tmp/flower.pri
+GIT_SSH_COMMAND="ssh -i /tmp/flower.pri" git push
 echo "Done!"
